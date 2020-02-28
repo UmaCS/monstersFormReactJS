c023beab2c15802e361fe60bb912032e32d16d0b
UmaCS <wiutumka@gmail.com>
2020-02-28 08:51:28 -0800

    final commit,the application is ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
 import './App.css';
 
@@ -14,14 +15,25 @@ class App extends Component{
    .then(response => response.json())
    .then(users => this.setState({monsters: users}))
  }
-  render(){
+
+handleChange = e => {
+  this.setState({searchField: e.target.value})
+}
+
+render(){
+
+    const {monsters,searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-      <input type='search'
-        placeholder='search monsters'
-        onChange={e => this.setState({searchField: e.target.value})}
+        <h1>Monsters Rolodex</h1>
+       <SearchBox
+         placeholder='search monsters'
+         handleChange={this.handleChange}
        />
-      <CardList monsters={this.state.monsters} />
+      <CardList monsters={filteredMonsters} />
       </div>
     );
   }
